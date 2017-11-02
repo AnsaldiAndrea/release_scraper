0b778f4a856fb61a1d0cb33cc2e12c91e520375c
Raistrike <32521260+Raistrike@users.noreply.github.com>
2017-11-02 20:50:50 +0100

    code clean-up

--- package/encoder.py
+++ package/encoder.py
@@ -1,7 +1,9 @@
+"""module for text encoding/decoding to/from base64 """
 import base64
 
 
 def encode(string, return_type='b'):
+    """encode string to base64"""
     if return_type is 'b':
         return base64.b64encode(string.encode('utf-8'))
     elif return_type is 'str':
@@ -10,8 +12,9 @@ def encode(string, return_type='b'):
 
 
 def decode(string, return_type='b'):
-    if return_type is 'b':
+    """decode string from base64"""
+    if return_type == 'b':
         return base64.b64decode(string)
-    elif return_type is 'str':
+    elif return_type == 'str':
         return base64.b64decode(string).decode('utf-8')
     raise Exception('invalid return_type')
